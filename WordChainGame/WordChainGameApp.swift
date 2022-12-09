51bfa9889e2905b73c53dbd85b15936166898e55
angry-dev <angrydev@naver.com>
2022-12-09 15:49:02 +0900

    Initial Commit

---
+++ WordChainGame/WordChainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordChainGameApp.swift
+//  WordChainGame
+//
+//  Created by 전혜성 on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct WordChainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
